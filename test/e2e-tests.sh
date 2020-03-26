ab67c64f2885d0886af0916d479e4fb80d564a41
Roland Huß <roland@ro14nd.de>
2020-03-26 18:03:35 +0100

    worked on

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -1,16 +1,21 @@
 # Dispatch tests to the different plugin directories
 
+
 run() {
+  local basedir=$(basedir)
+
+  # Environment variable which can be used my plugins
+  export TEST_INFRA_SCRIPTS="$basedir/test-infra/scripts"
+
+  source $TEST_INFRA_SCRIPTS/e2e-tests.sh
+
   # Iterate over all plugin directories and check whether they have testing
   # enabled
-  set -x
-
-  local $basedir=$(basedir)
   for plugin in $(ls $basedir/plugins); do
     local test_script=$basedir/plugins/"${plugin}"/test/e2e-tests.sh
     if [ -x $basedir/plugins/$plugin/test/e2e-tests.sh ]; then
       echo "== $plugin ==============================="
-      eval "source $test_script" || fail_test
+      eval "$test_script" || fail_test
       echo "=========================================="
     fi
   done
@@ -23,3 +28,5 @@ basedir() {
   local full_dir=$(cd "${dir}/.." && pwd)
   echo ${full_dir}
 }
+
+run $@
