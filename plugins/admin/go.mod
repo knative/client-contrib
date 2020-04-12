47f710a5c25ac2f3275788c276ad122c159ed4ed
Gong Zhang <zhanggbj@cn.ibm.com>
2020-04-12 17:10:09 +0800

    Restruct kn-admin plugin
    - To align with hello example plugin, restruct and add missing part
    - Add admin plugin maintainer in OWNERS

---
+++ plugins/admin/go.mod
@@ -0,0 +1,21 @@
+module knative.dev/client-contrib/plugins/admin
+
+go 1.13
+
+require (
+	github.com/google/go-cmp v0.3.1 // indirect
+	github.com/googleapis/gnostic v0.2.2 // indirect
+	github.com/imdario/mergo v0.3.8 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.6.2
+	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413 // indirect
+	golang.org/x/sys v0.0.0-20191010194322-b09406accb47 // indirect
+	google.golang.org/appengine v1.6.2 // indirect
+	gotest.tools v2.2.0+incompatible
+	k8s.io/api v0.17.3
+	k8s.io/apimachinery v0.17.3
+	k8s.io/client-go v0.17.0
+	k8s.io/utils v0.0.0-20200124190032-861946025e34 // indirect
+)
---
+++ plugins/admin/pkg/command/domain/domain.go
@@ -0,0 +1,38 @@
+// Copyright © 2020 The Knative Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package domain
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"knative.dev/client-contrib/plugins/admin/pkg"
+)
+
+// domainCmd represents the domain command
+func NewDomainCmd(p *pkg.AdminParams) *cobra.Command {
+	var domainCmd = &cobra.Command{
+		Use:   "domain",
+		Short: "Manage route domain",
+		Long: `Set default route domain or route domain for Service with selectors. For example:
+
+kn admin domain set - to set Knative route domain`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("domain called")
+		},
+	}
+	domainCmd.AddCommand(NewDomainSetCommand(p))
+	return domainCmd
+}
