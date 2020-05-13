909a4c3febbf5a5c8adba043ba600e8218a91457
Ying Chun Guo <guoyingc@cn.ibm.com>
2020-05-14 06:13:58 +0800

    add kafka source dependencies (#27)

---
+++ plugins/source-kafka/go.mod
@@ -0,0 +1,15 @@
+module knative.dev/client-contrib/plugins/source-kafka
+
+go 1.13
+
+require (
+	github.com/maximilien/kn-source-pkg v0.4.0
+	github.com/spf13/cobra v1.0.0 // indirect
+	knative.dev/eventing-contrib v0.14.0
+	knative.dev/test-infra v0.0.0-20200413202711-9cf64fb1b912
+)
+
+// Temporary pinning certain libraries. Please check periodically, whether these are still needed
+// ----------------------------------------------------------------------------------------------
+
+replace github.com/spf13/cobra => github.com/chmouel/cobra v0.0.0-20191021105835-a78788917390
---
+++ plugins/source-kafka/vendor/github.com/maximilien/kn-source-pkg/pkg/factories/boilerplate.go
@@ -0,0 +1,49 @@
+// Copyright © 2020 The Knative Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package factories
+
+import "github.com/maximilien/kn-source-pkg/pkg/types"
+
+// KnSourceFactory
+
+func (f *DefautKnSourceFactory) KnSourceParams() *types.KnSourceParams {
+	if f.knSourceParams == nil {
+		f.knSourceParams = f.CreateKnSourceParams()
+	}
+
+	return f.knSourceParams
+}
+
+// CommandFactory
+
+func (f *DefautCommandFactory) KnSourceFactory() types.KnSourceFactory {
+	return f.knSourceFactory
+}
+
+// FlagsFactory
+
+func (f *DefautFlagsFactory) KnSourceFactory() types.KnSourceFactory {
+	return f.knSourceFactory
+}
+
+// RunEFactory
+
+func (f *DefautRunEFactory) KnSourceFactory() types.KnSourceFactory {
+	return f.knSourceFactory
+}
+
+func (f *DefautRunEFactory) KnSourceClient(namespace string) types.KnSourceClient {
+	return f.knSourceFactory.CreateKnSourceClient(namespace)
+}
