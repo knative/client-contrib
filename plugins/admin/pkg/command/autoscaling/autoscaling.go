f207c1f9a74266bebeb5a2a32f2a51cf8f9cd87f
Gong Zhang <zhanggbj@cn.ibm.com>
2020-06-04 14:37:18 +0800

    Add support to enable or disable scale-to-zero (#26)
    - Init autoscaling subcommand
    - Support scale-to-zero config
    - Update examples in README

---
+++ plugins/admin/pkg/command/autoscaling/autoscaling.go
@@ -0,0 +1,34 @@
+// Copyright © 2020 The Knative Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package autoscaling
+
+import (
+	"github.com/spf13/cobra"
+	"knative.dev/client-contrib/plugins/admin/pkg"
+)
+
+// domainCmd represents the domain command
+func NewAutoscalingCmd(p *pkg.AdminParams) *cobra.Command {
+	var AutoscalingCmd = &cobra.Command{
+		Use:   "autoscaling",
+		Short: "Manage autoscaling config",
+		Long: `Manage autoscaling provided by Knative Pod Autoscaler (KPA). For example:
+
+kn admin autoscaling update - to manage autoscaling config`,
+	}
+	AutoscalingCmd.AddCommand(NewAutoscalingUpdateCommand(p))
+	AutoscalingCmd.InitDefaultHelpFlag()
+	return AutoscalingCmd
+}
